acd2cd370e24a0d86143b78b56cebe8a3e13dd50
putriauraa <putriauraramadhan7@gmail.com>
2025-10-21 14:30:59 +0800

    Update Form

--- LatihanGithub/FormJurusan.Designer.cs
+++ LatihanGithub/FormJurusan.Designer.cs
@@ -34,7 +34,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(338, 49);
+            this.label1.Location = new System.Drawing.Point(112, 110);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(51, 20);
             this.label1.TabIndex = 0;
